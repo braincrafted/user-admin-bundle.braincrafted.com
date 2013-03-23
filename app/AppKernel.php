666829323c569c22ddb374b5f9166f0d7244654c
Florian Eckerstorfer <florian@theroadtojoy.at>
2013-03-23 18:57:19 +0100

    Enabled bundles, basic config

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,10 @@ class AppKernel extends Kernel
             new JMS\AopBundle\JMSAopBundle(),
             new JMS\DiExtraBundle\JMSDiExtraBundle($this),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
+            new Bc\Bundle\BootstrapBundle\BcBootstrapBundle(),
+            new Bc\Bundle\UserBundle\BcUserBundle(),
+            new Bc\Bundle\UserAdminBundle\BcUserAdminBundle()
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
